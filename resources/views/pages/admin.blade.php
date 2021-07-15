f86d2caf48f6bb1d71733623a110a658d122de48
Nguyen Sy <syn282002@gmail.com>
2021-07-15 18:57:42 +0700

    change

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -5,6 +5,7 @@
 <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
 <script src="/assets_js/ckeditor/ckeditor.js"></script>
 <script src="/assets_js/ckeditor/samples/js/sample.js"></script>
+<link href="/assets4/css/write.css" rel="stylesheet">
 <script language="JavaScript" src="https://code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
 <script language="JavaScript" src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script language="JavaScript" src="https://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>

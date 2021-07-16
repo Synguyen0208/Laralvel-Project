4f24964382ef6da38ffc4bffb722b55bea96ecd4
unknown <admin@PNVL59.pn.com.vn>
2021-07-16 19:04:17 +0700

    gg

--- resources/js/Client/component/content.js
+++ resources/js/Client/component/content.js
@@ -5,8 +5,6 @@ import Partner from './content/partner';
 import Service from './content/service';
 import { Helmet } from 'react-helmet';
 import Key_date from './content/key_date';
-import Training from './content/training';
-import Help from './content/help';
 class Content extends Component {
     constructor(props) {
         super(props);
@@ -18,8 +16,6 @@ class Content extends Component {
             <main id="main">
                 <About/>
                 <Key_date/>
-                <Training/>
-                <Help/>
             </main>
         );
     }
